c0fb20f222d3acf58c31e53a85c68930a2cf0a6c
athiragithubworld <athirahrms@gmail.com>
2023-03-18 00:51:46 +0530

    SellingAdminPage.js

---
+++ src/Components/SellerAdminPage.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+
+const SellerAdminPage = () => {
+  const [productId, setproductId] = useState("");
+  const [sellingPrice, setsellingPrice] = useState("");
+  const [productName, setproductName] = useState("");
+
+  const [productList, setproductList] = useState([]);
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
+
+  
+  const productIdHandler = (event) => {
+    setproductId(event.target.value);
+  };
+
+  const sellingPriceHandler = (event) => {
+    setsellingPrice(event.target.value);
+  };
+
+  const totalprice = (event) =>{
+    var tprice =0;
+    var newprice=setsellingPrice(event.target.value)
+    tprice=tprice+newprice;
+  }
+
+  const productNameHandler = (event) => {
+    setproductName(event.target.value);
+  };
+
+  const productSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const productobj = {
+      productId,
+      sellingPrice,
+      productName,
+    };
+
+    setproductList((prevList) => {
+      const productList = [
+        ...prevList,
+        {
+          sellingPrice: sellingPrice,
+          productName: productName,
+          productId: productId,
+        },
+      ];
+      // console.log("tab",tab);
+      return productList;
+    });
+
+    localStorage.setItem(productobj.productId, JSON.stringify(productobj));
+
+    setproductId("");
+    setsellingPrice("");
+    setproductName("");
+  };
+
+  const productDeleteHandler = (productid) => {
+    setproductList();
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="productid">Product ID</label>
+        <input
+          type="number"
+          id="productid"
+          value={productId}
+          onChange={productIdHandler}
+        ></input>
+
+        <label htmlFor="sellingprice">Selling Price</label>
+        <input
+          type="number"
+          id="sellingprice"
+          value={sellingPrice}
+          onChange={sellingPriceHandler}
+        ></input>
+
+        <label htmlFor="productname">Product Name</label>
+        <input
+          type="text"
+          id="productname"
+          value={productName}
+          onChange={productNameHandler}
+        ></input>
+
+        <button type="submit" onClick={productSubmitHandler}>
+          {" "}
+          Add Product{" "}
+        </button>
+      </form>
+      <h2>Products</h2>
+      <div>
+        
+        <ul id="productList" className="productList">
+          {productList.map((product) => (
+            <li key={product.productId}>
+              {/* {sellingPrice:sellingPrice ,productName:productName,productId:productId} */}
+              {product.sellingPrice} - {product.productName}
+              <button onClick={() => productDeleteHandler(product.productId)}>
+                Delete
+              </button>
+            </li>
+            // totalprice = totalprice + {product.sellingPrice}
+          ))}
+        </ul>
+        {/* { var totalprice=0
+        totalprice=totalprice + {sellingPrice}} */}
+        
+        <label>Total Value worth of Products : {totalprice}</label>
+      </div>
+    </div>
+  );
+};
+
+export default SellerAdminPage;
