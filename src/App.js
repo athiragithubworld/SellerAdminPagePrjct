111ec7d76e173c758e8c642d3b8ba9fc5f43422a
athiragithubworld <athirahrms@gmail.com>
2023-03-18 00:49:38 +0530

    App.js file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SellerAdminPage from "./Components/SellerAdminPage";
+
+function App() {
+  return (
+    <div>
+      
+      <SellerAdminPage></SellerAdminPage>
+      {/* <h2>Let's get started!</h2> */}
+    </div>
+  );
+}
+
+export default App;
