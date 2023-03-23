c65d7b5df1559931c9c0118a025d4f9b7b63c9c7
athiragithubworld <athirahrms@gmail.com>
2023-03-23 10:45:26 +0530

    App.js file (Everything works perfectly)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React,{useState} from "react";
+
 import SellerAdminPage from "./Components/SellerAdminPage";
 
 function App() {
+  // const [keys, setKey] = useState(() => {
+  //   // getting stored value
+  //   const saved = localStorage.getItem("");
+  //   const initialValue = JSON.parse(saved);
+  //   return initialValue || "";
+  // });
   return (
     <div>
       
